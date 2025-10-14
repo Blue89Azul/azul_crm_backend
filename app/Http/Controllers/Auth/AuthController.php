376c77d213ddee6274e69ddb34a55b669555207e
azul <life.89.blue@gmail.com>
2025-10-14 09:58:24 -0300

    update/add Authentication Controller and Router

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signup(): void 
+    {
+        
+    }
+
+    public function loginWithEmail(): void
+    {
+
+    }
+
+    public function logout(): void
+    {
+        
+    }
+}
