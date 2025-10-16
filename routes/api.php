dba76823fc193f745a35405d88196065188f7fb8
azul <life.89.blue@gmail.com>
2025-10-16 10:37:21 -0300

    update/corrección de formato en la definición del grupo de rutas en api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function() {
+Route::controller(AuthController::class)->group(function () {
     Route::post('/signup', 'signup');
     Route::post('/login/email', 'loginWithEmail');
 });
-
