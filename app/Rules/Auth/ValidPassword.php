06b96ebb383bb97ee55042dfa7e7e03f032f02e1
azul <life.89.blue@gmail.com>
2025-10-30 15:41:44 -0300

    update/ajustar reglas de validación en ValidInvitationCode y ValidPassword para mejorar la verificación de entrada

--- app/Rules/Auth/ValidPassword.php
+++ app/Rules/Auth/ValidPassword.php
@@ -14,6 +14,23 @@ class ValidPassword implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+        if (!is_string($value)) {
+            $fail(':attribute は文字列で入力してください。');
+            return;
+        }
+
+        if (!preg_match('/^[\x20-\x7E]+$/', $value)) {
+            $fail(':attribute は半角英数字・半角記号のみ使用できます。');
+            return;
+        }
+
+        if (!preg_match('/[A-Za-z]/', $value)) {
+            $fail(':attribute: には英字を一文字以上含めてください。');
+        }
+
+        if (!preg_match('/\d/', $value)) {
+            $fail(':attribute には数字を1文字以上含めてください。');
+            return;
+        }
     }
 }
