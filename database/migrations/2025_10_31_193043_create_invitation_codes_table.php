55da3985468d09cd36316783e6e234230ed70364
azul <life.89.blue@gmail.com>
2025-11-05 10:25:21 -0300

    update/agregar migraciones para las tablas user_organizations e invitation_codes y columnas en users

---
+++ database/migrations/2025_10_31_193043_create_invitation_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitation_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 24)->unique()->comment('招待コード');
+            $table->foreignId('role_id')->nullable()->constrained('user_roles')->nullOnDelete();
+            $table->foreignId('organization_id')->nullable()->constrained('user_organizations')->nullOnDelete();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('used_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamp('redeemed_at')->nullable()->comment('招待コードが使用（償却）された日時');
+            $table->timestamp('expires_at')->nullable()->comment('有効期限');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitation_codes');
+    }
+};
---
+++ database/migrations/2025_10_31_194709_add_columns_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('invitation_code_id')->nullable()->constrained('invitation_codes')->nullOnDelete();
+            $table->foreignId('organization_id')->nullable()->constrained('user_organizations')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['invitation_code_id', 'organization_id']);
+        });
+    }
+};
