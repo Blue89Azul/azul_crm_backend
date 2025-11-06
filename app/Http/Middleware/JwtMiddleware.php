5536a33e8ffd774ef836805566461c6a13435490
azul <life.89.blue@gmail.com>
2025-11-06 11:32:04 -0300

    update/agregar middleware JwtMiddleware para autenticación JWT

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Responses\ApiErrorResponse;
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JwtMiddleware
+{
+    /**
+     * Authentication For JWT
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            return ApiErrorResponse::make(
+                'Authentication Failed',
+                $e->getMessage(),
+                400
+            )->toResponse();
+        }
+
+        return $next($request);
+    }
+}
