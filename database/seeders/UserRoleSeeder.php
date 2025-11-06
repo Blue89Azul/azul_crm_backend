956646c97376dda102a684e27f76ac47e01860a6
azul <life.89.blue@gmail.com>
2025-11-06 10:32:30 -0300

    update/agregar seeder para los roles de usuario

---
+++ database/seeders/UserRoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('user_roles')->insert([
+            ['role_name' => 'admin'],
+            ['role_name' => 'member']
+        ]);
+    }
+}
