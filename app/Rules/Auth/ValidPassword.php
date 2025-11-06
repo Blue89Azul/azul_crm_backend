19e53dae07fd306a13ed453a48475cc03d5366a4
azul <life.89.blue@gmail.com>
2025-11-06 11:10:06 -0300

    update/corrige mensajes de error en la regla de validación de contraseña

--- app/Rules/Auth/ValidPassword.php
+++ app/Rules/Auth/ValidPassword.php
@@ -15,21 +15,26 @@ class ValidPassword implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if (!is_string($value)) {
-            $fail(':attribute は文字列で入力してください。');
+            $fail(':attributeは文字列で入力してください。');
+            return;
+        }
+
+        if (strlen($value) < 8 || strlen($value) > 16) {
+            $fail(':attributeは8文字以上、16文字以下の文字列で入力してください。');
             return;
         }
 
         if (!preg_match('/^[\x20-\x7E]+$/', $value)) {
-            $fail(':attribute は半角英数字・半角記号のみ使用できます。');
+            $fail(':attributeは半角英数字・半角記号のみ使用できます。');
             return;
         }
 
         if (!preg_match('/[A-Za-z]/', $value)) {
-            $fail(':attribute: には英字を一文字以上含めてください。');
+            $fail(':attributeは英字を一文字以上含めてください。');
         }
 
         if (!preg_match('/\d/', $value)) {
-            $fail(':attribute には数字を1文字以上含めてください。');
+            $fail(':attributeは数字を1文字以上含めてください。');
             return;
         }
     }
