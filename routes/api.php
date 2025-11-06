c51bb3ba3060bcb6ce6a795448c8eb6fe968d56b
azul <life.89.blue@gmail.com>
2025-11-06 11:31:09 -0300

    update/reorganiza las rutas de autenticación y agrega el middleware jwt_auth

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\InvitationCode\InvitationCodeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/signup', 'signup');
-    Route::post('/login/email', 'loginWithEmail');
+});
+
+Route::middleware('jwt_auth')->group(function () {
+    Route::post('/invitation_codes', [InvitationCodeController::class, 'store']);
 });
