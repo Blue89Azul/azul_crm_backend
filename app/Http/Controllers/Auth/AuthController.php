9a78690a02ca2dd0397486d355534155a8e7b889
azul <life.89.blue@gmail.com>
2025-11-06 11:30:38 -0300

    update/mejora el manejo de errores en el método de registro y ajusta la validación de datos

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\SignUpRequest;
+use App\Http\Responses\ApiErrorResponse;
 use App\Services\Auth\AuthService;
 use Illuminate\Http\JsonResponse;
 use Tymon\JWTAuth\Exceptions\JWTException;
@@ -20,34 +22,35 @@ class AuthController extends Controller
      * signUp
      * @param \App\Http\Requests\Auth\SignUpRequest $request
      * @return JsonResponse
+     * 
+     * HTTPステータスをEnumで管理してもいいかも
      */
     public function signup(SignUpRequest $request): JsonResponse
     {
-        // バリデーション
         $validated_data = $request->validated();
 
         // ユーザー追加
         $user = $this->authService->signup(
             $validated_data['email'],
             $validated_data['password'],
-            $validated_data['invitation_code']
+            $validated_data['invitation_code'] ?? ""
         );
 
         // JWTトークンの作成
         try {
             $token = JWTAuth::fromUser($user);
         } catch (JWTException $e) {
-            return response()->json([
-                'token' => "error",
-            ]);
+            return ApiErrorResponse::make(
+                'トークン作成の失敗',
+                $e->getMessage(),
+                500,
+            )->toResponse();
         }
 
         return response()->json([
-            'token' => $token,
-        ]);
+            'token'      => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => 3600
+        ], 200);
     }
-
-    public function loginWithEmail(): void {}
-
-    public function logout(): void {}
 }
