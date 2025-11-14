94b94f0efa3b02030ec1e182f173cde6905bc559
azul <life.89.blue@gmail.com>
2025-11-14 09:25:58 -0300

    update/elimina importación innecesaria de Request y ajusta las rutas de códigos de invitación

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\InvitationCode\InvitationCodeController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
@@ -10,5 +9,6 @@ Route::controller(AuthController::class)->group(function () {
 });
 
 Route::middleware('jwt_auth')->group(function () {
-    Route::post('/invitation_codes', [InvitationCodeController::class, 'store']);
+    Route::get('/invitation_codes', [InvitationCodeController::class, 'fetchAllCodes']);
+    Route::post('/invitation_codes', [InvitationCodeController::class, 'createNewCode']);
 });
