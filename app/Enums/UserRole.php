e8ff0b72848f8157ea10297eab4230ca83806178
azul <life.89.blue@gmail.com>
2025-12-25 21:38:20 -0300

    update/agrega método isAdmin en el enum UserRole

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -5,7 +5,12 @@ namespace App\Enums;
 enum UserRole: string
 {
     case ADMIN  = "admin";
-    case MEMBER = "member"; 
+    case MEMBER = "member";
+
+    public function isAdmin(): bool
+    {
+        return $this === self::ADMIN;
+    }
 
     public function isMember(): bool
     {
