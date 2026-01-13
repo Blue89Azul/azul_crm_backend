eae9c9dd2271f28e6fef48f1c98710883bdd3880
azul <life.89.blue@gmail.com>
2026-01-13 08:48:55 -0300

    update/reorganiza las rutas de la API para agrupar el middleware de autenticación y mejorar la estructura

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,14 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\InvitationCode\InvitationCodeController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['tenant', 'jwt_auth'])->group(function () {
+    Route::get('/invitation_codes', [InvitationCodeController::class, 'fetchAllCodes']);
+    Route::post('/invitation_codes', [InvitationCodeController::class, 'createNewCode']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
 Route::controller(AuthController::class)->group(function () {
     Route::post('/signup', 'signup');
     Route::post('/login', 'login');
-    Route::post('/logout', 'logout');
-});
-
-Route::middleware('jwt_auth')->group(function () {
-    Route::get('/invitation_codes', [InvitationCodeController::class, 'fetchAllCodes']);
-    Route::post('/invitation_codes', [InvitationCodeController::class, 'createNewCode']);
 });
