43c13155108ed291b3219839cf60da95c2426c33
azul <life.89.blue@gmail.com>
2026-01-14 11:25:32 -0300

    update/agrega soporte para autenticación en la configuración de Scramble mediante claves de API y Bearer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Routing\Route;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
         Scramble::configure()
             ->routes(function (Route $route) {
                 return Str::startsWith($route->uri, 'api/');
+            })
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $tenantAuth = SecurityScheme::apiKey("header", "X-Tenant");
+                $bearerAuth = SecurityScheme::http("Bearer");
+
+                $openApi->secure($tenantAuth);
+                $openApi->secure($bearerAuth);
             });
     }
 }
